5460fbc544773d6cc9fa075bd46fb25b46a1c07f
Gabriel Luna <110780731+gaboslm@users.noreply.github.com>
2023-03-15 15:34:00 +0000

    vite config to build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+            return `love-roulette/assets/${extType}/[name]-[hash][extname]`;
+          }
+          return `love-roulette/assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'love-roulette/assets/[name]-[hash].js',
+        entryFileNames: 'love-roulette/assets/[name]-[hash].js',
+      },
+    },
+  },
 })
